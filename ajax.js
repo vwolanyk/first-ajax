a1755a9aebf3ab416c108347298d8234b2bc4602
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-07-12 11:24:06 -0400

    Exercise complete up to ping request

--- ajax.js
+++ ajax.js
@@ -1,5 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   /* Your code goes here */
+  var requestRoot = document.querySelector('button')
+  var requestPingPong = document.querySelector('button.pingpong')
+  requestRoot.addEventListener('click',function(){
+    $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'text',
+    });
+  });
+
+  requestPingPong.addEventListener('click', function(){
+    $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com/ping',
+    method: 'GET',
+    dataType: 'text',
+  }).done(function(responsedata){
+
+    console.log(responsedata);
+    step3456.append(responsedata);
+  });
+
+  });
+
 
 });
